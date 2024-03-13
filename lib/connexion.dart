baea4f953570ad7e2578851ee49b4f0b53feb5a5
khalil-45 <khalil.abada@etu.univ-orleans.fr>
2024-03-13 23:46:00 +0100

    page connexion presque fini

--- lib/connexion.dart
+++ lib/connexion.dart
@@ -1,15 +1,25 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:nice_buttons/nice_buttons.dart';
+import 'theme.dart' as theme;
+
 
 class Connexion extends StatelessWidget {
-  const Connexion({super.key});
+  const Connexion({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        fit: StackFit.expand,
-        children: [
-          Container(
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Container(
             margin: const EdgeInsets.only(top: 50.0),
             child: Column(
               children: [
@@ -25,8 +35,54 @@ class Connexion extends StatelessWidget {
               ],
             ),
           ),
-        ],
+              const SizedBox(height: 100.0), // Add some space between the text and input fields
+              const TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Email',
+                ),
+              ),
+              const SizedBox(height: 16.0), // Add some space between the input fields
+              const TextField(
+                obscureText: true,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Mot de passe',
+                ),
+              ),
+              const SizedBox(height: 16.0), // Add some space between the input fields and the button
+              buttonConnexion(context),
+            ],
+          ),
+        ),
       ),
     );
   }
+}
+
+NiceButtons buttonConnexion(BuildContext context) {
+return NiceButtons(
+            startColor: theme.Theme.primaryColor,
+            endColor: theme.Theme.primaryColor,
+            borderColor: theme.Theme.primaryColor,
+            stretch: false,
+            progress: true,
+            gradientOrientation: GradientOrientation.Horizontal,
+            onTap: (finish) {
+              Timer(const Duration(seconds: 4), () {
+                finish();
+                GoRouter.of(context).go('/home');
+              });
+            },
+            child: Text(
+              'Connexion',
+              style: GoogleFonts.sora(
+                textStyle: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          );
 }
--- lib/router.dart
+++ lib/router.dart
@@ -15,11 +15,36 @@ class AppRouter {
       ),
       GoRoute(
         path: '/connexion',
-        pageBuilder: (context, state) => const MaterialPage<void>(
-          key: ValueKey('/connexion'), 
+        pageBuilder: (context, state) => const MaterialPage(
+          key: ValueKey('/connexion'),
           child: Connexion(),
         ),
       ),
     ],
   );
+}
+
+class bottomNavBar extends StatelessWidget {
+  const bottomNavBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      items: const <BottomNavigationBarItem>[
+        BottomNavigationBarItem(
+          icon: Icon(Icons.home),
+          label: 'Home',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.business),
+          label: 'Business',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.school),
+          label: 'School',
+        ),
+      ],
+      selectedItemColor: Colors.amber[800],
+    );
+  }
 }
