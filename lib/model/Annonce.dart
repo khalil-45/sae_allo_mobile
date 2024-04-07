13055769e3f7828d14115370865a46a53780eeb7
Abdoulahi Fofana <abdoulahi.fofana@univ-orleans.fr>
2024-04-07 18:57:58 +0200

    class annonce

--- lib/model/Annonce.dart
+++ lib/model/Annonce.dart
@@ -1,71 +1,82 @@
 import 'package:sae_allo_mobile/model/Categorie.dart';
 import 'package:sae_allo_mobile/model/Etat.dart';
 import 'package:sae_allo_mobile/model/Utilisateurs.dart';
+import 'package:sae_allo_mobile/model/provider/CatProvider.dart';
+import 'package:sae_allo_mobile/model/provider/EtatProvider.dart';
+import 'package:sae_allo_mobile/model/provider/UserProvider.dart';
+
 
 class Annonce {
 
-  final int id_Annonce;
-  final Utilisateur util_Publieur;
-  String titre_Annonce;
-  String description_Annonce;
-  DateTime date_Annonce;
-  DateTime date_Fin_Annonce;
-  Etat etat;
+  final int idAnnonce;
+  final Utilisateur utilPublieur;
+  String titreAnnonce;
+  String descriptionAnnonce;
+  DateTime dateAnnonce;
+  String image;
+  DateTime dateFinAnnonce;
+  Etat etat ;
   Categorie categorie;
-  Utilisateur id_Util_Preneur;
+  int idUtilPreneur;
+  bool isFavorited = false;
 
   Annonce({
-    required this.id_Annonce,
-    required this.titre_Annonce,
-    required this.description_Annonce,
-    required this.date_Annonce,
-    required this.date_Fin_Annonce,
-    required this.etat,
+    required this.idAnnonce,
+    required this.titreAnnonce,
+    required this.descriptionAnnonce,
+    required this.dateAnnonce,
+    required this.dateFinAnnonce,
+    required this.etat ,
+    required this.image,
     required this.categorie,
-    required this.util_Publieur,
-    required this.id_Util_Preneur,
+    required this.utilPublieur,
+    required this.idUtilPreneur,
+    this.isFavorited = false,
   });
 
   Map<String, dynamic> toMap() {
     return {
-      'id_annonce': id_Annonce,
-      'titre_annonce': titre_Annonce,
-      'description_annonce': description_Annonce,
-      'date_annonce': date_Annonce,
-      'date_Fin_annonce': date_Fin_Annonce,
+      'id_annonce': idAnnonce,
+      'titre_annonce': titreAnnonce,
+      'description_annonce': descriptionAnnonce,
+      'date_annonce': dateAnnonce,
+      'date_Fin_annonce': dateFinAnnonce,
       'etat': etat.id_Etat,
       'categorie': categorie.id_Cat,
-      'util_publieur': util_Publieur.id_Util,
-      'id_Util_preneur': id_Util_Preneur
+      'util_publieur': utilPublieur.id_Util,
+      'id_Util_preneur': idUtilPreneur
     };
   }
 
   Map<String, dynamic> toInsert() {
     return {
-      'titre_annonce': titre_Annonce,
-      'description_annonce': description_Annonce,
-      'date_annonce': date_Annonce,
-      'date_fin_annonce': date_Fin_Annonce,
+      'titre_annonce': titreAnnonce,
+      'description_annonce': descriptionAnnonce,
+      'date_annonce': dateAnnonce,
+      'date_fin_annonce': dateFinAnnonce,
       'etat': etat.id_Etat,
       'categorie': categorie.id_Cat,
-      'util_Publieur': util_Publieur.id_Util,
-      'id_util_preneur': id_Util_Preneur
+      'util_Publieur': utilPublieur.id_Util,
+      'id_util_preneur': idUtilPreneur
     };
   }
 
-  // static Annonce fromMap(Map<String, dynamic> map) {
-  //   return Annonce(
-  //     id_Annonce: map['id_Annonce'],
-  //     titre_Annonce: map['titre_Annonce'],
-  //     description_Annonce: map['description_Annonce'],
-  //     date_Annonce: map['date_Annonce'],
-  //     date_Fin_Annonce: map['date_Fin_Annonce'],
-  //     etat: Etat.fromMap(map['etat']),
-  //     categorie: Categorie.fromMap(map['categorie']),
-  //     util_Publieur: Utilisateur.fromMap(map['util_Publieur']),
-  //     id_Util_Preneur: Utilisateur.fromMap(map['id_Util_Preneur']),
-  //   );
-  // }
+
+  factory Annonce.fromMap(Map<String, dynamic> map) {
+    return Annonce(
+      idAnnonce: map['id_annonce'],
+      titreAnnonce: map['titre_annonce'],
+      descriptionAnnonce: map['description_annonce'],
+      dateAnnonce: map['date_annonce'],
+      dateFinAnnonce: map['date_fin_annonce'],
+      etat: EtatProvider().getEtat(map['etat']),
+      categorie: cat_Provider().getCategorie(map['categorie']),
+      utilPublieur: UserProvider().getUser(map['util_publieur']),
+      idUtilPreneur: map['id_util_preneur'],
+      image: map['image'],
+    );
+  }
+
 
 
 
